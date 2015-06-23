931e6bd3e93091ef3083382da015672a2edb6c8a
Vihang D <vihang.dudhalkar@C02LV7CKFD56.local>
2015-06-23 14:05:02 +0530

    Updated hostnames

--- cookbooks/grid_zookeeper_deploy/attributes/default.rb
+++ cookbooks/grid_zookeeper_deploy/attributes/default.rb
@@ -1,3 +1,3 @@
 default['hdp']['version']='2.2.4.2-2'
 default['zookeeper']['zookeeper_version'] = '3.4.6.2.2.4.2-2'
-default['zookeeper']['quorum'] =  {'1' => 'tzns4006','2' => 'tzns4007','3' => 'tzns4010'}
+default['zookeeper']['quorum'] =  {'1' => 'tzkf4002','2' => 'tzkf4003','3' => 'tzkf4004'}
