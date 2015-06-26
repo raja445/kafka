dc06394e923b0b53a5f62551f2720052f4a2faa0
Vihang D <vihang.dudhalkar@C02LV7CKFD56.local>
2015-06-26 16:23:11 +0530

    Added a cookbook for kafka_mirroring

---
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -0,0 +1,2 @@
+default['kafkaMirroring']['version']= '0.2.11'
+default['kafkaMirroring']['instanceid']={ "tzkf4003" => 1 }
---
+++ cookbooks/kafka_mirroring/recipes/default.rb
@@ -0,0 +1,17 @@
+# could be improved to run only on update
+
+package ['kafkaMirroring','kafkaMirroringConfig'] do
+  version node['kafkaMirroring']['version']
+  action :install
+end
+
+template "/opt/inmobi/bin/KafkaMirroring/conf.sh" do
+  source "conf.sh.erb"
+  owner "root"
+  owner "root"
+  mode "0644"
+  variables(
+    :colo => node['fqdn'].split(".")[-3]
+    :instance_id => node['kafkaMirroring']['instanceid'][node['hostname']]
+  )
+end
