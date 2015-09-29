be06e2338bd5442833570207c3cdf191ab8847b4
Vihang D <vihangd@users.noreply.github.com>
2015-09-29 11:20:19 +0530

    Updated kmt nodes for uh1

--- cookbooks/kafka_mirroring/attributes/default.rb
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -1,2 +1,3 @@
 default['kafkaMirroring']['instanceid']['uj1']={ "tzkf4003" => 1, "tzkf4004" => 2  }
+default['kafkaMirroring']['instanceid']['uh1']={ "krkf4003" => 1, "krkf4004" => 2  }
 default['kafkaMirroring']['version']= '0.4.9'
