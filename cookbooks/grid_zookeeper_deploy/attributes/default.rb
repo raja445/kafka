79498581c6faa9cc7fec4d155c83403c68bc7be8
Vihang D <vihangd@users.noreply.github.com>
2015-09-30 16:46:03 +0530

    Updated kafka attributes

--- cookbooks/grid_zookeeper_deploy/attributes/default.rb
+++ cookbooks/grid_zookeeper_deploy/attributes/default.rb
@@ -2,5 +2,8 @@ default['hdp']['version']='2.2.4.2-2'
 default['zookeeper']['zookeeper_version'] = '3.4.6.2.2.4.2-2'
 default['zookeeper']['quorum']['uj1'] =  {'1' => 'tzkf4002','2' => 'tzkf4003','3' => 'tzkf4004'}
 default['zookeeper']['quorum']['uh1'] =  {'1' => 'krkf4002','2' => 'krkf4003','3' => 'krkf4004'}
+default['zookeeper']['quorum']['hkg1'] =  {'1' => 'opkf4002','2' => 'opkf4003','3' => 'opkf4004'}
+default['zookeeper']['quorum']['lhr1'] =  {'1' => 'erkf4002','2' => 'erkf4003','3' => 'erkf4004'}
 default['zookeeper']['log_dir'] = '/var/log/zookeeper'
-default['cluster_name'] = {'uj1'=> 'UJ1_Topaz', 'uh1' => 'UH1_Krypton'}
+default['zookeeper']['lib_dir'] = '/var/lib/zookeeper'
+default['cluster_name'] = {'uj1'=> 'UJ1_Topaz', 'uh1' => 'UH1_Krypton', 'hkg1' => 'HKG1_Opal', 'lhr1' => 'LHR1_Emerald'}
