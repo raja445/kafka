6a18ba92f590e333c8ec32a44a8ce445329508cc
Vihang D <vihangd@users.noreply.github.com>
2015-10-13 14:27:54 +0530

    Updated audit_worker recipe

--- cookbooks/audit_worker/recipes/default.rb
+++ cookbooks/audit_worker/recipes/default.rb
@@ -2,3 +2,14 @@ package ['KafkaAudit'] do
   version node['KafkaAudit']['version']
   action :install
 end
+
+template "/opt/inmobi/bin/AuditWorker/conf.sh" do
+  source "conf.sh.erb"
+  owner "root"
+  owner "root"
+  mode "0644"
+  colo = node['domain'].split(".")[-3]
+  variables(
+    :colo => colo,
+  )
+end
