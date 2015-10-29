586e0b88f83e7e4d38e4ed6cd4343b475ae3a150
Vihang D <vihangd@users.noreply.github.com>
2015-10-29 13:13:23 +0530

    Updated kafka attributes

--- cookbooks/kafka_mirroring/attributes/default.rb
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -1,4 +1,4 @@
-default['kafkaMirroring']['instanceid']['uj1']={ "tzkf4003" => 1, "tzkf4004" => 2  }
+default['kafkaMirroring']['instanceid']['uj1']={ "tzkf4006" => 1, "tzkf4007" => 2  }
 default['kafkaMirroring']['instanceid']['uh1']={ "krkf4003" => 1, "krkf4004" => 2  }
 default['kafkaMirroring']['instanceid']['hkg1']={ "opkf4003" => 1, "opkf4004" => 2  }
 default['kafkaMirroring']['instanceid']['lhr1']={ "erkf4003" => 1, "erkf4004" => 2  }
