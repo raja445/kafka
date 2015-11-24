75b5c414282a442b5d2cb132eaad34eb7aaaa66c
Sooraj S <sooraj.s@inmobi.com>
2015-11-24 15:13:32 +0530

    kafka upgrade 0.8.2.1 to 0.8.2.2

--- cookbooks/kafka/attributes/default.rb
+++ cookbooks/kafka/attributes/default.rb
@@ -6,7 +6,7 @@ default["kafka"]["user"] = "kafka"
 default["kafka"]["group"] = "kafka"
 
 default["kafka"]["scala_version"] = "2.11"
-default["kafka"]["version"] = "0.8.2.1"
+default["kafka"]["version"] = "0.8.2.2"
 default["kafka"]["download_url"] = "https://archive.apache.org/dist/kafka"
 
 default["kafka"]["base_dir"]  = "/opt/inmobi"
