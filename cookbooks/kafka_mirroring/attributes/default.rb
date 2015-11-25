a701db846995375785200af62320428744501b2c
Sooraj S <sooraj.s@inmobi.com>
2015-11-25 11:25:20 +0530

    KMT release

--- cookbooks/kafka_mirroring/attributes/default.rb
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -2,5 +2,5 @@ default['kafkaMirroring']['instanceid']['uj1']={ "tzkf4006" => 1, "tzkf4007" => 
 default['kafkaMirroring']['instanceid']['uh1']={ "krkf4003" => 1, "krkf4004" => 2  }
 default['kafkaMirroring']['instanceid']['hkg1']={ "opkf4003" => 1, "opkf4004" => 2  }
 default['kafkaMirroring']['instanceid']['lhr1']={ "erkf4003" => 1, "erkf4004" => 2  }
-default['kafkaMirroring']['version']= '0.4.26'
-default['kafkaMirroringConfig']['version']= '0.4.26'
+default['kafkaMirroring']['version']= '0.4.27'
+default['kafkaMirroringConfig']['version']= '0.4.27'
