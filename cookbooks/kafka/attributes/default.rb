466c8b5fa42ac410ac9d9446df6dff995af26fbd
Sooraj S <sooraj.s@inmobi.com>
2015-12-28 11:44:47 +0530

    GRIDOPS-6057

--- cookbooks/kafka/attributes/default.rb
+++ cookbooks/kafka/attributes/default.rb
@@ -18,16 +18,16 @@ default["kafka"]["service"]["stderr"] = File.join node["kafka"]["log_dir"], "kaf
 
 # These are required to be supplied by the consumer so setting to nil
 default["kafka"]["brokers"]["uj1"] = ["tzkf4005.grid.uj1.inmobi.com","tzkf4006.grid.uj1.inmobi.com","tzkf4007.grid.uj1.inmobi.com", "tzkf4008.grid.uj1.inmobi.com","tzkf4004.grid.uj1.inmobi.com","tzkf4009.grid.uj1.inmobi.com","tzkf4010.grid.uj1.inmobi.com","tzkf4011.grid.uj1.inmobi.com","tzkf4012.grid.uj1.inmobi.com","tzkf4013.grid.uj1.inmobi.com"]
-default["kafka"]["zookeepers"]["uj1"] = ["tzkf4005.grid.uj1.inmobi.com:2181","tzkf4006.grid.uj1.inmobi.com:2181","tzkf4007.grid.uj1.inmobi.com:2181","tzkf4008.grid.uj1.inmobi.com:2181","tzkf4009.grid.uj1.inmobi.com:2181"]
+default["kafka"]["zookeepers"]["uj1"] = ["kafka-zookeeper.grid.uj1.inmobi.com:2181"]
 
 default["kafka"]["brokers"]["uh1"] = ["krkf4002.grid.uh1.inmobi.com","krkf4003.grid.uh1.inmobi.com","krkf4004.grid.uh1.inmobi.com", "krkf4005.grid.uh1.inmobi.com","krkf4001.grid.uh1.inmobi.com","krkf4006.grid.uh1.inmobi.com","krkf4007.grid.uh1.inmobi.com","krkf4008.grid.uh1.inmobi.com","krkf4009.grid.uh1.inmobi.com","krkf4010.grid.uh1.inmobi.com"]
-default["kafka"]["zookeepers"]["uh1"] = ["krkf4002.grid.uh1.inmobi.com:2181","krkf4003.grid.uh1.inmobi.com:2181","krkf4004.grid.uh1.inmobi.com:2181","krkf4001.grid.uh1.inmobi.com:2181","krkf4005.grid.uh1.inmobi.com:2181"]
+default["kafka"]["zookeepers"]["uh1"] = ["kafka-zookeeper.grid.uh1.inmobi.com:2181"]
 
 default["kafka"]["brokers"]["hkg1"] = ["opkf4002.grid.hkg1.inmobi.com","opkf4003.grid.hkg1.inmobi.com","opkf4004.grid.hkg1.inmobi.com", "opkf4005.grid.hkg1.inmobi.com","opkf4001.grid.hkg1.inmobi.com","opkf4006.grid.hkg1.inmobi.com","opkf4007.grid.hkg1.inmobi.com","opkf4008.grid.hkg1.inmobi.com","opkf4009.grid.hkg1.inmobi.com","opkf4010.grid.hkg1.inmobi.com"]
-default["kafka"]["zookeepers"]["hkg1"] = ["opkf4002.grid.hkg1.inmobi.com:2181","opkf4003.grid.hkg1.inmobi.com:2181","opkf4004.grid.hkg1.inmobi.com:2181","opkf4001.grid.hkg1.inmobi.com:2181","opkf4005.grid.hkg1.inmobi.com:2181"]
+default["kafka"]["zookeepers"]["hkg1"] = ["kafka-zookeeper.grid.hkg1.inmobi.com:2181"]
 
 default["kafka"]["brokers"]["lhr1"] = ["erkf4002.grid.lhr1.inmobi.com","erkf4003.grid.lhr1.inmobi.com","erkf4004.grid.lhr1.inmobi.com", "erkf4005.grid.lhr1.inmobi.com","erkf4001.grid.lhr1.inmobi.com","erkf4006.grid.lhr1.inmobi.com","erkf4007.grid.lhr1.inmobi.com","erkf4008.grid.lhr1.inmobi.com","erkf4009.grid.lhr1.inmobi.com","erkf4010.grid.lhr1.inmobi.com"]
-default["kafka"]["zookeepers"]["lhr1"] = ["erkf4002.grid.lhr1.inmobi.com:2181","erkf4003.grid.lhr1.inmobi.com:2181","erkf4004.grid.lhr1.inmobi.com:2181","erkf4001.grid.lhr1.inmobi.com:2181","erkf4005.grid.lhr1.inmobi.com:2181"]
+default["kafka"]["zookeepers"]["lhr1"] = ["kafka-zookeeper.grid.lhr1.inmobi.com:2181"]
 
 
 
