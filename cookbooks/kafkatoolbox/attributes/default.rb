c6214c443f0fda022d3c2448d89777f56e6c5b1c
Sooraj S <sooraj.s@inmobi.com>
2016-01-09 17:42:02 +0530

    GRIDOPS-6329

--- cookbooks/kafkatoolbox/attributes/default.rb
+++ cookbooks/kafkatoolbox/attributes/default.rb
@@ -1,1 +1,4 @@
-default["kafka"]["kafkatools_version"] = "1.1.1"
+default["kafkatoolbox"]["kafkatools_version"] = "1.1.1"
+default["kafkatoolbox"]["kafkatools_cache_dir"] = "/var/cache/kafkatoolbox"
+default["kafkatoolbox"]["download_url"] = "http://glvm1014.grid.uh1.inmobi.com/tar/kafkatoolbox/"
+default["kafkatoolbox"]["base_dir"] = "/opt/inmobi/bin"
--- cookbooks/kafkatoolbox/recipes/default.rb
+++ cookbooks/kafkatoolbox/recipes/default.rb
@@ -1,4 +1,34 @@
-package "kafkatoolbox" do
-  version node['kafkatools_version']
-  action :install
+directory "#{node["kafkatoolbox"]["kafkatools_cache_dir"]}" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
 end
+
+toolboxDirname = "KafkaToolbox-#{node["kafkatoolbox"]["kafkatools_version"]}"
+
+binFileName="KafkaToolbox-#{node["kafkatoolbox"]["kafkatools_version"]}.tar.gz"
+node.default["kafkatoolbox"]["binary_url"] = "#{node["kafkatoolbox"]["download_url"]}/#{binFileName}"
+node.default["kafkatoolbox"]["install_dir"] = "#{node["kafkatoolbox"]["base_dir"]}/KafkaToolbox"
+# Download kafkatoolbox tzr file if it does not exist already in cache directory
+remote_file "#{node["kafkatoolbox"]["kafkatools_cache_dir"]}/#{binFileName}" do
+  action :create_if_missing
+  source node["kafkatoolbox"]["binary_url"]
+  mode 00644
+  backup false
+end
+
+execute "untar kafkatoolbox binary" do
+  cwd node["kafkatoolbox"]["base_dir"]
+  command "tar zxf #{node["kafkatoolbox"]["kafkatools_cache_dir"]}/#{binFileName}"
+  not_if do
+    File.exists? File.join node["kafkatoolbox"]["base_dir"], toolboxDirname
+  end
+end
+
+link node["kafkatoolbox"]["install_dir"] do
+  owner 'root'
+  group 'root'
+  to File.join node["kafkatoolbox"]["base_dir"], toolboxDirname
+end
+
