a2cb05aaade2c30b222ba0b719d60963d4796546
Sooraj S <sooraj.s@inmobi.com>
2016-02-15 11:55:01 +0530

    kafka logrotate

--- cookbooks/logrotate/recipes/default.rb
+++ cookbooks/logrotate/recipes/default.rb
@@ -19,6 +19,13 @@ directory "#{node["logrotate"]["audit_logrotatedir"]}" do
   recursive true
   action :create
 end
+directory "#{node["logrotate"]["jmxtrans_logrotatedir"]}" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
 
 cookbook_file '/etc/logrotate.d/kafka.conf' do
   source 'kafka_all_logrotate.conf'
