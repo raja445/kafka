7e4f5d1243b56d235120f418f68f6f10c4417fcc
Sooraj S <sooraj.s@inmobi.com>
2016-02-23 14:33:19 +0530

    KAFKA-95

--- cookbooks/scandium/recipes/default.rb
+++ cookbooks/scandium/recipes/default.rb
@@ -15,3 +15,13 @@ template "/opt/inmobi/bin/Scandium/conf.sh" do
     :colo => colo,
   )
 end
+template "/opt/inmobi/config/ScandiumConfig/conf.sh" do
+  source "conf.sh.erb"
+  owner "root"
+  owner "root"
+  mode "0644"
+  colo = node['domain'].split(".")[-3]
+  variables(
+    :colo => colo,
+  )
+end
