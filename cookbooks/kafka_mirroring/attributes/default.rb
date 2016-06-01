8f48bc1081f57924a9c17325f9fb8ea9b03a2272
Sooraj S <sooraj.s@inmobi.com>
2016-06-01 15:53:22 +0530

    GRIDOPS-7229

--- cookbooks/kafka_mirroring/attributes/default.rb
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -1,5 +1,5 @@
 default['kafkaMirroring']['instanceid']['uj1']={ "tzkf4016" => 1, "tzkf4017" => 2, "tzkf4018" => 3  }
-default['kafkaMirroring']['instanceid']['uh1']={ "krkf4012" => 1, "krkf4013" => 2, "krkf4014" => 3  }
+default['kafkaMirroring']['instanceid']['uh1']={ "krkf4012" => 1, "krkf4011" => 2}
 default['kafkaMirroring']['instanceid']['hkg1']={ "opkf4012" => 1, "opkf4013" => 2, "opkf4014" => 3  }
 default['kafkaMirroring']['instanceid']['lhr1']={ "erkf4012" => 1, "erkf4013" => 2, "erkf4014" => 3  }
 default['kafkaMirroring']['version']= '0.5.75'
