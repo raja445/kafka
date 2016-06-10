9c08b805685e5cb7fda46845048a1622c09de789
sunil.kalva <sunil.kalva@inmobi.com>
2016-06-10 10:15:16 +0530

    version bump up

--- cookbooks/kafka_mirroring/attributes/default.rb
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -2,8 +2,8 @@ default['kafkaMirroring']['instanceid']['uj1']={ "tzkf4016" => 1, "tzkf4017" => 
 default['kafkaMirroring']['instanceid']['uh1']={ "krkf4012" => 1, "krkf4011" => 2, "krkf4013" => 3}
 default['kafkaMirroring']['instanceid']['hkg1']={ "opkf4013" => 1, "opkf4014" => 2, "opkf4015" => 3  }
 default['kafkaMirroring']['instanceid']['lhr1']={ "erkf4012" => 1, "erkf4013" => 2, "erkf4014" => 3  }
-default['kafkaMirroring']['version']= '0.5.83'
-default['kafkaMirroringConfig']['version']= '0.5.83'
+default['kafkaMirroring']['version']= '0.5.84'
+default['kafkaMirroringConfig']['version']= '0.5.84'
 default['kafkaMirroringConfig']['prodconf']= '/opt/inmobi/config/KafkaMirroringConfig/prod/'
 default['kafkaMirroringConfig']['confighome']= '/opt/inmobi/config/KafkaMirroringConfig/'
 default['kafkaSupervisorConfig']['confighome']= '/etc/supervisor/conf.d'
