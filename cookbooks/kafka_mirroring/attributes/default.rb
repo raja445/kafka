379b69d112c347025f8a5323e9f0ef618644066d
Sooraj S <sooraj.s@inmobi.com>
2016-07-28 00:06:59 +0530

    GRIDOPS-7552

--- cookbooks/kafka_mirroring/attributes/default.rb
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -3,8 +3,8 @@ default['kafkaMirroring']['instanceid']['dfw1']={ "oxkm4001" => 1}
 default['kafkaMirroring']['instanceid']['uh1']={ "krkf4012" => 1, "krkf4011" => 2, "krkf4013" => 3, "krkf4014" => 4}
 default['kafkaMirroring']['instanceid']['hkg1']={ "opkf4013" => 1, "opkf4014" => 2, "opkf4015" => 3  }
 default['kafkaMirroring']['instanceid']['lhr1']={ "erkf4012" => 1, "erkf4013" => 2, "erkf4014" => 3  }
-default['kafkaMirroring']['version']= '0.5.90'
-default['kafkaMirroringConfig']['version']= '0.5.90'
+default['kafkaMirroring']['version']= '0.5.91'
+default['kafkaMirroringConfig']['version']= '0.5.91'
 default['kafkaMirroring']['staging_dir']= '/data/kafka/staging'
 default['kafkaMirroringConfig']['prodconf']= '/opt/inmobi/config/KafkaMirroringConfig/prod/'
 default['kafkaMirroringConfig']['confighome']= '/opt/inmobi/config/KafkaMirroringConfig/'
