c2c86edf806daf28e63a1055d841295802d17cfb
Sooraj S <sooraj.s@inmobi.com>
2016-09-15 16:31:16 +0530

    GRIDOPS-7722

---
+++ cookbooks/perftools/recipes/default.rb
@@ -0,0 +1,29 @@
+# coding: UTF-8
+perfbin = "#{node["perftools"]["base_dir"]}/perf-tools-master/bin"
+remote_file "#{node["perftools"]["zipfile_path"]}" do
+  action :create_if_missing
+  source node["perftools"]["download_url"]
+  mode 00644
+  backup false
+end
+
+directory "#{node["perftools"]["base_dir"]}" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+execute "unzip perftools binary" do
+  cwd node["perftools"]["base_dir"]
+  command "unzip #{node["perftools"]["zipfile_path"]}"
+  not_if do
+    File.exists? "#{node["perftools"]["base_dir"]}/perf-tools-master"
+  end
+end
+
+link "/usr/bin/cachestat" do
+  to "#{perfbin}/cachestat"
+  link_type :symbolic
+end
+
