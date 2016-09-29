8914923a8d4e3a82af75389e091a80167f840c0a
vignesh.gs <vignesh.gs@inmobi.com>
2016-09-29 12:16:02 +0530

    [lhr1flumejmx]

--- cookbooks/flume_jmxtrans/attributes/default.rb
+++ cookbooks/flume_jmxtrans/attributes/default.rb
@@ -7,4 +7,5 @@ default["kafka"]["package_url"] = "http://glvm1014.grid.uh1.inmobi.com/deb/jmxtr
 default['kafka']['mrelay']['hkg1'] = "metrics-relay.grid.hkg1.inmobi.com"
 default['kafka']['mrelay']['uh1'] = "metrics-relay.grid.uh1.inmobi.com"
 default['kafka']['mrelay']['dfw1'] = "metrics-relay.grid.dfw1.inmobi.com"
+default['kafka']['mrelay']['lhr1'] = "metrics-relay.grid.lhr1.inmobi.com"
 default['cluster_name'] = {'dfw1' => 'DFW1_Onyx', 'uh1' => 'UH1_Krypton', 'hkg1' => 'HKG1_Opal', 'lhr1' => 'LHR1_Emerald'}
