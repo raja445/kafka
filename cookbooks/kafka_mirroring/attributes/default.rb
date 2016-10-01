1c714b95e5976a7cfd204cba5e85eeab4897e10b
Sooraj S <sooraj.s@inmobi.com>
2016-10-01 06:24:37 +0530

    GRIDOPS-7937

--- cookbooks/kafka_mirroring/attributes/default.rb
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -1,8 +1,8 @@
 default['kafkaMirroring']['instanceid']['uj1']={ "tzkf4016" => 1, "tzkf4017" => 2, "tzkf4018" => 3  , "tzkf4015" => 4}
 default['kafkaMirroring']['instanceid']['dfw1']={ "oxkm4001" => 1}
-default['kafkaMirroring']['instanceid']['uh1']={ "krkf4012" => 1, "krkf4011" => 2, "krkf4013" => 3, "krkf4014" => 4}
-default['kafkaMirroring']['instanceid']['hkg1']={ "opkf4013" => 1, "opkf4014" => 2, "opkf4015" => 3  }
-default['kafkaMirroring']['instanceid']['lhr1']={ "erkf4012" => 1, "erkf4013" => 2, "erkf4014" => 3  }
+default['kafkaMirroring']['instanceid']['uh1']={ "krkf4012" => 1}
+default['kafkaMirroring']['instanceid']['hkg1']={ "opkf4014" => 1}
+default['kafkaMirroring']['instanceid']['lhr1']={ "erkf4012" => 1 }
 default['kafkaMirroring']['version']= '0.5.93'
 default['kafkaMirroringConfig']['version']= '0.5.93'
 default['kafkaMirroring']['staging_dir']= '/data/kafka/staging'
