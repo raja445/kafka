5431c380fe64bfc86f4bbbac3dd50de40d8714f5
Vignesh G S <vignesh.gs@im2090-x2.corp.inmobi.com>
2016-10-17 21:19:47 +0530

    [GRIDOPS-8004]

--- cookbooks/flume_mid_collector/attributes/default.rb
+++ cookbooks/flume_mid_collector/attributes/default.rb
@@ -7,6 +7,7 @@ default["flume_collector"]["base_dir"]  = "/opt/inmobi"
 default["flume_collector"]["spool_dir"]  = "/data/d1/flume/spool"
 default["flume_collector"]["pid_dir"]  = "/var/run/flume"
 default["flume_collector"]["kafka_brokers"]['hkg1']  = "opkf4001.grid.hkg1.inmobi.com:9092,opkf4002.grid.hkg1.inmobi.com:9092,opkf4003.grid.hkg1.inmobi.com:9092,opkf4004.grid.hkg1.inmobi.com:9092,opkf4005.grid.hkg1.inmobi.com:9092,opkf4006.grid.hkg1.inmobi.com:9092,opkf4007.grid.hkg1.inmobi.com:9092,opkf4008.grid.hkg1.inmobi.com:9092,opkf4009.grid.hkg1.inmobi.com:9092,opkf4010.grid.hkg1.inmobi.com:9092"
+default["flume_collector"]["kafka_brokers"]['uh1']  = "krkf4001.grid.uh1.inmobi.com:9092,krkf4002.grid.uh1.inmobi.com:9092,krkf4003.grid.uh1.inmobi.com:9092,krkf4004.grid.uh1.inmobi.com:9092,krkf4005.grid.uh1.inmobi.com:9092,krkf4006.grid.uh1.inmobi.com:9092,krkf4007.grid.uh1.inmobi.com:9092,krkf4008.grid.uh1.inmobi.com:9092,krkf4009.grid.uh1.inmobi.com:9092,krkf4010.grid.uh1.inmobi.com:9092"
 
 #Configure the sources for the Flume Collector
 default["flume_collector"]["midcollector_sources"]['hkg1']  = {
@@ -27,8 +28,61 @@ default["flume_collector"]["midcollector_sources"]['hkg1']  = {
          :port => "2541"}
        }
 
+#Configure the sources for the UH1 Flume Collector
+default["flume_collector"]["midcollector_sources"]['uh1']  = {
+     "avrosrc" => {
+         :type => "avro",
+         :channels => "spillable1 spillable2 spillable3 spillable4 spillable5 spillable6",
+         :'selector.type' => "org.apache.flume.channel.RoundRobinChannelSelector",
+         :bind => "0.0.0.0",
+         :'enable_compression' => false,
+         :port => "2540"},
+     "zipavrosrc" => {
+         :type => "avro",
+         :channels => "spillable1 spillable2 spillable3 spillable4 spillable5 spillable6",
+         :'selector.type' => "org.apache.flume.channel.RoundRobinChannelSelector",
+         :bind => "0.0.0.0",
+         :'enable_compression' => true,
+         :'compression-type' => 'deflate',
+         :port => "2541"}
+       }
+
 #Configure the channels for the Flume Collector
 default["flume_collector"]["midcollector_channels"]['hkg1']  = {
+     "spillable1" => {
+         :type => "org.apache.flume.channel.DiskBackedMemoryChannel",
+         :capacity => "40000",
+         :transactionCapacity => "500",
+         :spoolDiskCapacityMB => "1000000"},
+     "spillable2" => {
+         :type => "org.apache.flume.channel.DiskBackedMemoryChannel",
+         :capacity => "40000",
+         :transactionCapacity => "500",
+         :spoolDiskCapacityMB => "1000000"},
+     "spillable3" => {
+         :type => "org.apache.flume.channel.DiskBackedMemoryChannel",
+         :capacity => "40000",
+         :transactionCapacity => "500",
+         :spoolDiskCapacityMB => "1000000"},
+     "spillable4" => {
+         :type => "org.apache.flume.channel.DiskBackedMemoryChannel",
+         :capacity => "40000",
+         :transactionCapacity => "500",
+         :spoolDiskCapacityMB => "1000000"},
+     "spillable5" => {
+         :type => "org.apache.flume.channel.DiskBackedMemoryChannel",
+         :capacity => "40000",
+         :transactionCapacity => "500",
+         :spoolDiskCapacityMB => "1000000"},
+     "spillable6" => {
+         :type => "org.apache.flume.channel.DiskBackedMemoryChannel",
+         :capacity => "40000",
+         :transactionCapacity => "500",
+         :spoolDiskCapacityMB => "1000000"}
+         }
+
+#Configure the channels for the UH1 Flume Collector
+default["flume_collector"]["midcollector_channels"]['uh1']  = {
      "spillable1" => {
          :type => "org.apache.flume.channel.DiskBackedMemoryChannel",
          :capacity => "40000",
@@ -112,6 +166,65 @@ default["flume_collector"]["midcollector_sinks"]['hkg1']  = {
          :type => "avro",
          :channel => "spillable6",
          :hostname => "flume.grid.hkg1.inmobi.com",
+         :port => "2541",
+         :'enable_compression' => true,
+         :'compression_type' => "deflate",
+         :'batch-size' => "500",
+         :'reset-connection-interval' => "60"}
+         }
+
+
+#Configure the sinks for the Flume Collector
+default["flume_collector"]["midcollector_sinks"]['uh1']  = {
+     "avrosink1" => {
+         :type => "avro",
+         :channel => "spillable1",
+         :hostname => "flume.grid.uh1.inmobi.com",
+         :port => "2541",
+         :'enable_compression' => true,
+         :'compression_type' => "deflate",
+         :'batch-size' => "500",
+         :'reset-connection-interval' => "60"},
+     "avrosink2" => {
+         :type => "avro",
+         :channel => "spillable2",
+         :hostname => "flume.grid.uh1.inmobi.com",
+         :port => "2541",
+         :'enable_compression' => true,
+         :'compression_type' => "deflate",
+         :'batch-size' => "500",
+         :'reset-connection-interval' => "60"},
+     "avrosink3" => {
+         :type => "avro",
+         :channel => "spillable3",
+         :hostname => "flume.grid.uh1.inmobi.com",
+         :port => "2541",
+         :'enable_compression' => true,
+         :'compression_type' => "deflate",
+         :'batch-size' => "500",
+         :'reset-connection-interval' => "60"},
+     "avrosink4" => {
+         :type => "avro",
+         :channel => "spillable4",
+         :hostname => "flume.grid.uh1.inmobi.com",
+         :port => "2541",
+         :'enable_compression' => true,
+         :'compression_type' => "deflate",
+         :'batch-size' => "500",
+         :'reset-connection-interval' => "60"},
+     "avrosink5" => {
+         :type => "avro",
+         :channel => "spillable5",
+         :hostname => "flume.grid.uh1.inmobi.com",
+         :port => "2541",
+         :'enable_compression' => true,
+         :'compression_type' => "deflate",
+         :'batch-size' => "500",
+         :'reset-connection-interval' => "60"},
+     "avrosink6" => {
+         :type => "avro",
+         :channel => "spillable6",
+         :hostname => "flume.grid.uh1.inmobi.com",
          :port => "2541",
          :'enable_compression' => true,
          :'compression_type' => "deflate",
