14a37a81bde60d5ff380184c32e320b41fb37dc9
Sooraj S <sooraj.s@inmobi.com>
2016-11-09 17:53:11 +0530

    kmt rollback to 0.6.1 from 0.6.10

--- cookbooks/kafka_mirroring/attributes/default.rb
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -3,8 +3,8 @@ default['kafkaMirroring']['instanceid']['dfw1']={ "oxkm4001" => 1,"oxkm4003" => 
 default['kafkaMirroring']['instanceid']['uh1']={ "krkf4012" => 1, "krkf4011" => 2}
 default['kafkaMirroring']['instanceid']['hkg1']={ "opkf4014" => 1, "opkf4016" => 2}
 default['kafkaMirroring']['instanceid']['lhr1']={ "erkf4012" => 1, "erkf4014" => 2}
-default['kafkaMirroring']['version']= '0.6.10'
-default['kafkaMirroringConfig']['version']= '0.6.10'
+default['kafkaMirroring']['version']= '0.6.1'
+default['kafkaMirroringConfig']['version']= '0.6.1'
 default['kafkaMirroring']['staging_dir']= '/data/kafka/staging'
 default['kafkaMirroringConfig']['prodconf']= '/opt/inmobi/config/KafkaMirroringConfig/prod/'
 default['kafkaMirroringConfig']['confighome']= '/opt/inmobi/config/KafkaMirroringConfig/'
