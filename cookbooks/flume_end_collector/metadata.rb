a4717751b73fce5fdbfdd86f9017380e19a9ef26
Sooraj S <sooraj.s@inmobi.com>
2017-02-27 00:20:40 +0530

    merge source templating

--- cookbooks/flume_end_collector/metadata.rb
+++ cookbooks/flume_end_collector/metadata.rb
@@ -1,4 +1,4 @@
-name              "flume_collector"
+name              "flume_end_collector"
 maintainer        "gridops"
 maintainer_email  "gridops@imobi.com"
 description       'Installs and configures Flume Collector'
