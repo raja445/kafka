ded8ac7c4181d44d0dc6dae5bfcdd18f7f78cd3c
Sooraj S <sooraj.s@inmobi.com>
2017-04-12 12:11:15 +0530

    azure kafka

---
+++ cookbooks/grid_zookeeper_deploy_azure/metadata.rb
@@ -0,0 +1,6 @@
+name              "grid_zookeeper_deploy_azure"
+maintainer        "GRIDOPS"
+maintainer_email  "grid-ops@inmobi.com"
+description       "Installs zookeeper"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.1"
