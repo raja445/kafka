706f777107b4b3ef9feee9d53e13089afac8dcb2
Sooraj S <sooraj.s@inmobi.com>
2017-04-21 17:01:44 +0530

    azure flume deployment

--- cookbooks/azure_flume_collector/recipes/default.rb
+++ cookbooks/azure_flume_collector/recipes/default.rb
@@ -100,3 +100,52 @@ link "#{flumeHome}" do
   link_type :symbolic
 end
 
+
+
+#HEALTH CHECK
+package "scribe-scripts" do
+  version node['flume_mid_collector']['scribescripts']
+  action :install
+  options '--force-yes'
+end
+
+directory "/etc/flume_health" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+directory "/etc/flume_health/flume_healthcheck" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+directory "/etc/flume_health/flume_healthcheck/log" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+directory "/etc/flume_health/flume_healthcheck/log/main" do
+  owner 'nobody'
+  group 'nogroup'
+  mode '0755'
+  action :create
+end
+template "/etc/flume_health/flume_healthcheck/run" do
+  source "flume_healthcheck.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+template "/etc/flume_health/flume_healthcheck/log/run" do
+  source "flume_logrun.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+link '/service/flume_healthcheck' do
+  to '/etc/flume_health/flume_healthcheck'
+end
