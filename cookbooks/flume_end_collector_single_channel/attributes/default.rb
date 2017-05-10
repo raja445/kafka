ca28fdee88b9e968bb757861176bb97ef2f56cce
Sanooj Mananghat <sanooj.mananghat@inmobi.com>
2017-05-10 11:47:56 +0530

    GRIDOPS-9278 Flume upgrade

--- cookbooks/flume_end_collector_single_channel/attributes/default.rb
+++ cookbooks/flume_end_collector_single_channel/attributes/default.rb
@@ -47,21 +47,21 @@ default["flume_collector"]["mergesrc_consumer_gpsize"]['hkg1']  = "6"
 default["flume_collector"]["endcollector_local_retention_topics"]['uh1']  = "testmerge,rr"
 default["flume_collector"]["endcollector_merge_retention_topics"]['uh1']  = "beacon_rr_uh1_cpm_render"
 
-default["flume_collector"]["endcollector_all_channels"]['uh1']  = "spillable1 spillable2 spillable3 spillable4 spillable5 spillable6 spillable7 spillable8 spillable9 spillable10 spillable11 spillable12 spillable13 spillable14 spillable15 spillable16 spillable17 spillable18 spillable19 spillable20 spillable21 spillable22 spillable23 spillable24 spillable25 spillable26 spillable27 spillable28 spillable29 spillable30 spillable31 spillable32 spillable33 spillable34 spillable35 spillable36 spillable37 spillable38  mergespillable1 mergespillable2 uh1-to-uh1-channel1 uh1-to-uh1-channel2 uh1-to-dfw1-channel1 uh1-to-dfw1-channel2 uh1-to-lhr1-channel1 uh1-to-lhr1-channel2 uh1-to-pek1-channel1 uh1-to-pek1-channel2 hdfsmerge-channel1 hdfsmerge-channel2 hdfslocal-channel1 hdfslocal-channel2"
+default["flume_collector"]["endcollector_all_channels"]['uh1']  = "spillable mergespillable uh1-to-uh1-channel1 uh1-to-uh1-channel2 uh1-to-dfw1-channel1 uh1-to-dfw1-channel2 uh1-to-lhr1-channel1 uh1-to-lhr1-channel2 uh1-to-pek1-channel1 uh1-to-pek1-channel2 hdfsmerge-channel  hdfslocal-channel"
 
-default["flume_collector"]["endcollector_all_sinks"]['uh1'] = "kafkasink1 kafkasink2 kafkasink3 kafkasink4 kafkasink5 kafkasink6 kafkasink7 kafkasink8 kafkasink9 kafkasink10 kafkasink11 kafkasink12 kafkasink13 kafkasink14 kafkasink15 kafkasink16 kafkasink17 kafkasink18 kafkasink19 kafkasink20 kafkasink21 kafkasink22 kafkasink23 kafkasink24 kafkasink25 kafkasink26 kafkasink27 kafkasink28 kafkasink29 kafkasink30 kafkasink31 kafkasink32 kafkasink33 kafkasink34 kafkasink35 kafkasink36 kafkasink37 kafkasink38 mergekafkasink1 mergekafkasink2 uh1-mergesink1 uh1-mergesink2 dfw1-mergesink1 dfw1-mergesink2 lhr1-mergesink1 lhr1-mergesink2 pek1-mergesink1 pek1-mergesink2 hdfsmerge-sink1 hdfsmerge-sink2 hdfslocal-sink1 hdfslocal-sink2"
+default["flume_collector"]["endcollector_all_sinks"]['uh1'] = "kafkasink mergekafkasink uh1-mergesink1 uh1-mergesink2 dfw1-mergesink1 dfw1-mergesink2 lhr1-mergesink1 lhr1-mergesink2 pek1-mergesink1 pek1-mergesink2 hdfsmerge-sink hdfslocal-sink"
 #Configure the sources for the Flume Collector
 default["flume_collector"]["endcollector_sources"]['uh1']  = {
      "avrosrc" => {
          :src_category => "avro",
          :type => "avro",
-         :channels => "spillable1 spillable2 spillable3 spillable4 spillable5 spillable6 spillable7 spillable8 spillable9 spillable10 spillable11 spillable12 spillable13 spillable14 spillable15 spillable16 spillable17 spillable18 spillable19 spillable20 spillable21 spillable22 spillable23 spillable24 spillable25 spillable26 spillable27 spillable28 spillable29 spillable30 spillable31 spillable32 spillable33 spillable34 spillable35 spillable36 spillable37 spillable38",
+         :channels => "spillable",
          :'selector.type' => "org.apache.flume.channel.RoundRobinChannelSelector",
          :port => "2540"},
      "zipavrosrc" => {
          :src_category => "avro",
          :type => "avro",
-         :channels => "spillable1 spillable2 spillable3 spillable4 spillable5 spillable6 spillable7 spillable8 spillable9 spillable10 spillable11 spillable12 spillable13 spillable14 spillable15 spillable16 spillable17 spillable18 spillable19 spillable20 spillable21 spillable22 spillable23 spillable24 spillable25 spillable26 spillable27 spillable28 spillable29 spillable30 spillable31 spillable32 spillable33 spillable34 spillable35 spillable36 spillable37 spillable38",
+         :channels => "spillable",
          :'selector.type' => "org.apache.flume.channel.RoundRobinChannelSelector",
          :'enable_compression' => true,
          :'compression-type' => 'deflate',
@@ -69,7 +69,7 @@ default["flume_collector"]["endcollector_sources"]['uh1']  = {
      "mergezipavrosrc" => {
          :src_category => "avro",
          :type => "avro",
-         :channels => "mergespillable1 mergespillable2",
+         :channels => "mergespillable",
          :'selector.type' => "org.apache.flume.channel.RoundRobinChannelSelector",
          :'enable_compression' => true,
          :'compression-type' => 'deflate',
@@ -78,7 +78,7 @@ default["flume_collector"]["endcollector_sources"]['uh1']  = {
          :src_category => "hdfslocal",
          :consumer_group => "uh1hdfslocal",
          :type => "org.apache.flume.source.kafka.MultiKafkaSource",
-         :channels => "hdfslocal-channel1 hdfslocal-channel2",
+         :channels => "hdfslocal-channel",
          :'selector.type' => "org.apache.flume.channel.RoundRobinChannelSelector",
          :batchSize => 500,
          :'kafka.topics' => "testmerge,rr"},
@@ -86,7 +86,7 @@ default["flume_collector"]["endcollector_sources"]['uh1']  = {
          :src_category => "hdfsmerge",
          :consumer_group => "uh1hdfsmerge",
          :type => "org.apache.flume.source.kafka.MultiKafkaSource",
-         :channels => "hdfsmerge-channel1 hdfsmerge-channel2",
+         :channels => "hdfsmerge-channel",
          :'selector.type' => "org.apache.flume.channel.RoundRobinChannelSelector",
          :batchSize => 500,
          :'kafka.topics' => "merge_beacon_rr_uh1_cpm_render"},
@@ -125,59 +125,21 @@ default["flume_collector"]["endcollector_sources"]['uh1']  = {
        }
 
 #Configure the channels for the Flume Collector
-default["flume_collector"]["endcollector_normal_avroreceive_channels"]['uh1']  = ["spillable1","spillable2","spillable3","spillable4","spillable5","spillable6","spillable7","spillable8","spillable9","spillable10","spillable11","spillable12","spillable13","spillable14","spillable15","spillable16","spillable17","spillable18","spillable19","spillable20","spillable21","spillable22","spillable23","spillable24","spillable25","spillable26","spillable27","spillable28","spillable29","spillable30","spillable31","spillable32","spillable33","spillable34","spillable35","spillable36","spillable37","spillable38"]
-default["flume_collector"]["endcollector_merge_avroreceive_channels"]['uh1']  = ["mergespillable1","mergespillable2"]
+default["flume_collector"]["endcollector_normal_avroreceive_channels"]['uh1']  = ["spillable"]
+default["flume_collector"]["endcollector_merge_avroreceive_channels"]['uh1']  = ["mergespillable"]
 default["flume_collector"]["endcollector_merge_kafkaread_channels"]['uh1']  = ["uh1-to-uh1-channel1","uh1-to-uh1-channel2","uh1-to-dfw1-channel1","uh1-to-dfw1-channel2","uh1-to-lhr1-channel1","uh1-to-lhr1-channel2","uh1-to-pek1-channel1","uh1-to-pek1-channel2",]
-default["flume_collector"]["endcollector_merge_hdfs_channels"]['uh1']  = ["hdfsmerge-channel1","hdfsmerge-channel2"]
-default["flume_collector"]["endcollector_local_hdfs_channels"]['uh1']  = ["hdfslocal-channel1","hdfslocal-channel2"]
+default["flume_collector"]["endcollector_merge_hdfs_channels"]['uh1']  = ["hdfsmerge-channel"]
+default["flume_collector"]["endcollector_local_hdfs_channels"]['uh1']  = ["hdfslocal-channel"]
 
 
 #Configure the sinks for the Flume Collector
 default["flume_collector"]["endcollector_normal_kafka_sinks"]['uh1']  = {
-     "kafkasink1" => {:channel => "spillable1"},
-     "kafkasink2" => {:channel => "spillable2"},
-     "kafkasink3" => {:channel => "spillable3"},
-     "kafkasink4" => {:channel => "spillable4"},
-     "kafkasink5" => {:channel => "spillable5"},
-     "kafkasink6" => {:channel => "spillable6"},
-     "kafkasink7" => {:channel => "spillable7"},
-     "kafkasink8" => {:channel => "spillable8"},
-     "kafkasink9" => {:channel => "spillable9"},
-     "kafkasink10" => {:channel => "spillable10"},
-     "kafkasink11" => {:channel => "spillable11"},
-     "kafkasink12" => {:channel => "spillable12"},
-     "kafkasink13" => {:channel => "spillable13"},
-     "kafkasink14" => {:channel => "spillable14"},
-     "kafkasink15" => {:channel => "spillable15"},
-     "kafkasink16" => {:channel => "spillable16"},
-     "kafkasink17" => {:channel => "spillable17"},
-     "kafkasink18" => {:channel => "spillable18"},
-     "kafkasink19" => {:channel => "spillable19"},
-     "kafkasink20" => {:channel => "spillable20"},
-     "kafkasink21" => {:channel => "spillable21"},
-     "kafkasink22" => {:channel => "spillable22"},
-     "kafkasink23" => {:channel => "spillable23"},
-     "kafkasink24" => {:channel => "spillable24"},
-     "kafkasink25" => {:channel => "spillable25"},
-     "kafkasink26" => {:channel => "spillable26"},
-     "kafkasink27" => {:channel => "spillable27"},
-     "kafkasink28" => {:channel => "spillable28"},
-     "kafkasink29" => {:channel => "spillable29"},
-     "kafkasink30" => {:channel => "spillable30"},
-     "kafkasink31" => {:channel => "spillable31"},
-     "kafkasink32" => {:channel => "spillable32"},
-     "kafkasink33" => {:channel => "spillable33"},
-     "kafkasink34" => {:channel => "spillable34"},
-     "kafkasink35" => {:channel => "spillable35"},
-     "kafkasink36" => {:channel => "spillable36"},
-     "kafkasink37" => {:channel => "spillable37"},
-     "kafkasink38" => {:channel => "spillable38"}
+     "kafkasink" => {:channel => "spillable"}
 }
 
 
 default["flume_collector"]["endcollector_merged_kafka_sinks"]['uh1']  = {
-     "mergekafkasink1" => {:channel => "mergespillable1"},
-     "mergekafkasink2" => {:channel => "mergespillable2"}
+     "mergekafkasink" => {:channel => "mergespillable"}
 }
 
 default["flume_collector"]["endcollector_merged_avro_sinks"]['uh1']  = {
