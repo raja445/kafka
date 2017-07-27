3d52e368c4f619f85a8b5a217f970502851c9880
Sooraj S <sooraj.s@inmobi.com>
2017-07-28 01:43:50 +0530

    tpce streams to uh1 gold

---
+++ cookbooks/kmt_collector_gold/metadata.rb
@@ -0,0 +1,6 @@
+name              "kmt_collector_gold"
+maintainer        "GRIDOPS"
+maintainer_email  "grid-ops@inmobi.com"
+description       "Installs KMTCollector"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.1"
