c44211b62fa60e8e31bf87f4f3719117bd1f9b58
Vignesh G S <vignesh.gs@im2090-x3.corp.inmobi.com>
2017-08-11 14:23:22 +0530

    [kmt minutly node addtion for dfw1 beefup]

--- cookbooks/kafka_mirroring/attributes/default.rb
+++ cookbooks/kafka_mirroring/attributes/default.rb
@@ -1,4 +1,4 @@
-default['kafkaMirroring']['instanceid']['dfw1']={ "oxkm4001" => 1,"oxkm4003" => 2,"oxkm4004" => 3}
+default['kafkaMirroring']['instanceid']['dfw1']={ "oxkm4001" => 1,"oxkm4003" => 2,"oxkm4004" => 3,"oxkm4005" =>4}
 default['kafkaMirroring']['instanceid']['uh1']={ "krkf4012" => 2, "krkf4011" => 3, "krkf4015" => 1, "krkf4016" => 4}
 default['kafkaMirroring']['instanceid']['lhr1']={ "erkf4012" => 1, "erkf4014" => 2}
 default['kafkaMirroring']['instanceid']['pek1']={ "pykm4002" => 1, "pykm4003" => 2}
