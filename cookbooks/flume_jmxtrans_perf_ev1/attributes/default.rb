b3fc6a793b273cf7588fd9c71dbe069f4280d76c
Sooraj S <sooraj.s@inmobi.com>
2017-09-09 13:24:18 +0530

    GRIDOPS-10298

---
+++ cookbooks/flume_jmxtrans_perf_ev1/attributes/default.rb
@@ -0,0 +1,9 @@
+# coding: UTF-8 
+# Cookbook Name:: kafka
+# Attributes:: default
+
+default["flume_jmxtrans"]["package"] = "jmxtrans-263.deb"
+default["kafka"]["package_url"] = "http://glvm1014.grid.uh1.inmobi.com/deb/jmxtrans-263.deb"
+default['kafka']['mrelay']['dfw1'] = "metrics-relay.grid.dfw1.inmobi.com"
+default['kafka']['mrelay']['ev1'] = "metrics-relay.grid.dfw1.inmobi.com"
+default['cluster_name'] = {'dfw1' => 'DFW1_Onyx', 'ev1' => 'DFW1_Onyx'}
