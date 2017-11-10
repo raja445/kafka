df68650f7f949189f1a8b32ecc4e5262442c0435
Vignesh G S <vignesh.gs@inmobi.com>
2017-11-10 08:41:41 +0530

    [jmxtrans]

--- cookbooks/flume_jmxtrans/attributes/default.rb
+++ cookbooks/flume_jmxtrans/attributes/default.rb
@@ -3,7 +3,7 @@
 # Attributes:: default
 
 default["flume_jmxtrans"]["package"] = "jmxtrans-253.deb"
-default["kafka"]["package_url"] = "http://glvm1014.grid.uh1.inmobi.com/deb/jmxtrans-253.deb"
+default["kafka"]["package_url"] = "http://plgw4001.grid.dfw1.inmobi.com/deb/jmxtrans-253.deb"
 default['kafka']['mrelay']['hkg1'] = "metrics-relay.grid.hkg1.inmobi.com"
 default['kafka']['mrelay']['uh1'] = "metrics-relay.grid.uh1.inmobi.com"
 default['kafka']['mrelay']['dfw1'] = "metrics-relay.grid.dfw1.inmobi.com"
