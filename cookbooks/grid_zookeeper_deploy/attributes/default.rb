db6adcc05c9485f794a4ba96bead2161a2ed42cb
Prathyush Babu S <prathyush.s@im2361-x0.corp.inmobi.com>
2017-12-11 11:11:31 +0530

    azure kafka

--- cookbooks/grid_zookeeper_deploy/attributes/default.rb
+++ cookbooks/grid_zookeeper_deploy/attributes/default.rb
@@ -7,6 +7,7 @@ default['zookeeper']['quorum']['lhr1'] =  {'1' => 'erkf4002','2' => 'erkf4003','
 default['zookeeper']['quorum']['dfw1'] =  {'1' => 'oxkf4001','2' => 'oxkf4002','3' => 'oxkf4003','4' => 'oxkf4008','5' => 'oxkf4009'}
 default['zookeeper']['quorum']['pek1'] =  {'1' => 'pykf4001','2' => 'pykf4002','3' => 'pykf4003','4' => 'pykf4004','5' => 'pykf4005'}
 default['zookeeper']['quorum']['dfw2'] =  {'1' => 'gakf4001','2' => 'gakf4002','3' => 'gakf4003','4' => 'gakf4004','5' => 'gakf4005'}
+default['zookeeper']['quorum']['ams1'] =  {'1' => 'azkf4001','2' => 'azkf4002','3' => 'azkf4003','4' => 'azkf4004','5' => 'azkf4005'}
 default['zookeeper']['log_dir'] = '/var/log/zookeeper'
 default['zookeeper']['lib_dir'] = '/var/lib/zookeeper'
-default['cluster_name'] = { 'lhr1' => 'LHR1_Emerald', 'dfw1' => 'DFW1_Onyx', 'pek1' => 'pek1-pyrite' , 'dfw2' => 'dfw2-garnet'}
+default['cluster_name'] = { 'lhr1' => 'LHR1_Emerald', 'dfw1' => 'DFW1_Onyx', 'pek1' => 'pek1-pyrite' , 'dfw2' => 'dfw2-garnet' , 'ams1' => 'ams1-azurite'}
