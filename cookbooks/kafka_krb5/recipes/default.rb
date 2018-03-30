fe23d50fee3256352439a8d834b818f8ce6e379f
Sooraj S <sooraj.s@inmobi.com>
2018-03-30 14:49:29 +0530

    dfw2 kafka io threads increase after raid0

--- cookbooks/kafka_krb5/recipes/default.rb
+++ cookbooks/kafka_krb5/recipes/default.rb
@@ -68,6 +68,8 @@ end
 node.default['kafka']['server.properties']['log.dir'] = node['kafkadisks']['mounts'].strip
 if cluster_colo1 == 'ams1'
   node.default['kafka']['server.properties']['num.io.threads'] = node['kafkadisks']['mounts'].split(",").length()*36
+elsif cluster_colo1 == 'dfw2'
+  node.default['kafka']['server.properties']['num.io.threads'] = node['kafkadisks']['mounts'].split(",").length()*40
 else
   node.default['kafka']['server.properties']['num.io.threads'] = node['kafkadisks']['mounts'].split(",").length()*4
 end
