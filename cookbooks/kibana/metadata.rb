572e53ba7869564f1e46b625f45d203089d12c34
Vignesh G S <vignesh.gs@IM2090-L0.corp.inmobi.com>
2018-05-14 18:05:52 +0530

    [elk]

---
+++ cookbooks/kibana/metadata.rb
@@ -0,0 +1,6 @@
+name              "kibana"
+maintainer        "gridops"
+maintainer_email  "gridops@imobi.com"
+description       'Installs and configures kibana'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.1.0'
