32f2a3f6ebb4bf462f2f267325ae34e8576de5a0
Vignesh G S <vignesh.gs@IM2090-L0.corp.inmobi.com>
2018-05-16 14:12:16 +0530

    [elk]

--- cookbooks/elasticsearch/attributes/default.rb
+++ cookbooks/elasticsearch/attributes/default.rb
@@ -4,10 +4,10 @@ default["elasticsearch"]["base_dir"]  = "/opt/inmobi"
 default["elasticsearch"]["log_base_dir"]  = "/var/log"
 
 #elastic search node
-default["elasticsearch"]["data_nodes"]['dfw2'] = ["grid-staging1.grid.dfw2.inmobi.com","grid-staging2.grid.dfw2.inmobi.com","grid-staging3.grid.dfw2.inmobi.com"]
+default["elasticsearch"]["data_nodes"]['dfw2'] = ["elk1001.grid.dfw2.inmobi.com","elk1002.grid.dfw2.inmobi.com","elk1003.grid.dfw2.inmobi.com","elk1004.grid.dfw2.inmobi.com"]
 
 #minimun master node 
 #    (no of nodes/2) + 1
 # E.g 3 elastic search nodes 
 # (3/2)+1 = 2
-default["elasticsearch"]["minimum_master_node"]['dfw2'] = 2
+default["elasticsearch"]["minimum_master_node"]['dfw2'] = 3
