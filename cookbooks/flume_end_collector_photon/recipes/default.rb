bbf2c635dc31ad17e279122b95c70d4fc62c40d0
Prathyush Babu S <prathyush.s@im2361-x0.corp.inmobi.com>
2018-10-05 20:06:12 +0530

    [GRIDOPS-12739] DFW2 eventhub setup for inorganic-install

--- cookbooks/flume_end_collector_photon/recipes/default.rb
+++ cookbooks/flume_end_collector_photon/recipes/default.rb
@@ -89,6 +89,7 @@ template "#{flumeConf}/flume-end-collector.properties" do
     :local_eventhub_sinks4 =>node["flume_collector"]["endcollector_local_eventhub_sinks4"]['dfw1'],
     :local_eventhub_sinks5 =>node["flume_collector"]["endcollector_local_eventhub_sinks5"]['dfw1'],
     :local_eventhub_sinks1 =>node["flume_collector"]["endcollector_local_eventhub_sinks1"]['dfw2'],
+    :local_eventhub_sinks2 =>node["flume_collector"]["endcollector_local_eventhub_sinks2"]['dfw2'],
     :local_hdfs_channels =>node["flume_collector"]["endcollector_local_hdfs_channels"][colo],
     :local_eventhub_channels =>node["flume_collector"]["endcollector_local_eventhub_channels"]['dfw1'],
     :local_eventhub_channels =>node["flume_collector"]["endcollector_local_eventhub_channels"]['dfw2'],
